8ec7e05f0bc73bbf19dda0ff74e46e342a18e0fd
mikhail <mikhail.volkov.135@gmail.com>
2020-01-30 17:59:17 +0200

    added sities list

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import style from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
 export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isUkraine: true,
+            isWorld: false
+        }
+    }
+
+    handleClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            isUkraine: !this.state.isUkraine,
+            isWorld: !this.state.isWorld
+        })
+    }
+
     render() {
         return (
-            <ul className={style.header}>
-                <a href="">
-                    <li>Home</li>
-                </a>
-                <a href="">
-                    <li>Booking hotel</li>
-                </a>
-                <a href="">
-                    <li>About us</li>
-                </a>
-                <a href="">
-                    <li>Contacts</li>
-                </a>
-            </ul>
+            <header>
+                <ul className={style.header}>
+                    <li><a href="" onClick={this.handleClick}>Cities of the Ukraine</a></li>
+                    <li><a href="" onClick={this.handleClick}>Cities of the World</a></li>
+                </ul>
+                <div className={this.state.isUkraine ? style.hide : style.sitiesUkraine}>
+                    <NavLink to='/'>Kharkiv</NavLink>
+                    <NavLink to='/'>Kyev</NavLink>
+                    <NavLink to='/'>Donetsk</NavLink>
+                    <NavLink to='/'>Luhansk</NavLink>
+                    <NavLink to='/'>Vinnytsia</NavLink>
+                    <NavLink to='/'>Zhytomyr</NavLink>
+                    <NavLink to='/'>Kirovohrad</NavLink>
+                    <NavLink to='/'>Lutsk</NavLink>
+                    <NavLink to='/'>Lviv</NavLink>
+                    <NavLink to='/'>Odesa</NavLink>
+                </div>
+                <div className={this.state.isWorld ? style.hide : style.sitiesWorlds}>
+                    <NavLink to='/'>Paris</NavLink>
+                    <NavLink to='/'>Riga</NavLink>
+                    <NavLink to='/'>Donetsk</NavLink>
+                    <NavLink to='/'>Luhansk</NavLink>
+                    <NavLink to='/'>Vinnytsia</NavLink>
+                    <NavLink to='/'>Zhytomyr</NavLink>
+                    <NavLink to='/'>Kirovohrad</NavLink>
+                    <NavLink to='/'>Lutsk</NavLink>
+                    <NavLink to='/'>Lviv</NavLink>
+                    <NavLink to='/'>Odesa</NavLink>
+                </div>
+            </header>
         )
     }
 }
